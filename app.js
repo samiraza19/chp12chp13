4b30478482661c7bd9edf1cdacbf5d4269dfd113
=SamiRaza <=samirazagb@gmail.com>
2023-09-13 22:44:52 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,75 @@
+// Q 1
+var enti1 = prompt("Enter a value like A=65")
+if(enti1 ==="A=65"){
+alert("This is upercase letter")    
+}
+
+var enti2 = prompt("Enter a value like 62")
+if(enti2 >60){
+alert("This is a number")    
+}
+
+var enti3 = prompt("Enter a value like D=38")
+if(enti3 ==="D=38"){
+alert("This is upercase letter")    
+}
+
+var enti4 = prompt("Enter a value like c=93")
+if(enti4 ==="c=93"){
+alert("This is lowercase letter")    
+}
+
+//Q 2
+var bravo = prompt("Enter a value of integers")
+if(bravo >0){
+    alert("This value is equal")
+}else{
+    alert("this is not equal")
+}
+
+var posi =prompt("Enter a positive value")
+if (posi >0){
+    alert("This is positive value")
+}
+else{
+    alert("This is not a positive value")
+}
+
+var vowel = prompt ("Enter a vowel word")
+if (vowel ="a ,e ,i ,o ,u")
+{
+    alert("This is vowel later")
+}else{
+    alert("This is not a positive value")
+}
+
+var password = prompt("Enter a correct password like xyz")
+if(password ==="xyz"){
+    alert("Please enter your password")
+}
+if(password ==="SamiRaza")
+{alert("Correct! The Password you entered matches to original password")}
+else{
+    alert("Incorrect password")
+} 
+
+var greeting;
+var hour = prompt("Enter an hour in 24 hours format") ;
+if (hour < 18){ 
+alert =("Good day") ;}
+else{alert("Good evening")}
+
+var timing1 = prompt("Enter 24 hour format timing")
+if(timing1 >=1000 && timing1<1200  ){
+    alert("Good morning")
+}
+if(timing1 >=1200 && timing1<1700  ){
+    alert("Good afternoon")
+}
+if(timing1 >=1700 && timing1<2100  ){
+    alert("Good evenibg")
+}
+if(timing1 >=2100 && timing1<2359  ){
+    alert("Good night")
+}
+
